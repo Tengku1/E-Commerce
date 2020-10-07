9a60837de664db512ead804389d03a47251619e8
Tengku1 <anountengku@gmail.com>
2020-10-07 09:58:47 +0800

    First App, add home page, detail page, shop page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{asset('Bootstrap4/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
-    <link rel="stylesheet" href="{{asset('font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('Bootstrap4/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('font-awesome-4.7.0/css/font-awesome.min.css')}}">
     @yield('Othercss')
     <title>{{$title ?? 'Putri'}}</title>
 </head>
